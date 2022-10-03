87fd482045fdd7d3becf9836f86751af4bad47a8
Manon Brun <manonbrun0613@gmail.com>
2022-10-03 16:21:09 +0200

    Limit game to 5 rounds and touch up interface

--- script.js
+++ script.js
@@ -30,32 +30,57 @@ function getPlayerChoice() {
 }
 
 const result = document.querySelector('.result')
-const score = document.querySelector('.score')
+let scoreuser = document.querySelector('.user')
+let scorecomputer = document.querySelector('.computer')
+let userScore = 0
+let computerScore=0
+scorecomputer.textContent = 'Computer : ' +computerScore;
+scoreuser.textContent = 'User : '+userScore;
+
+let numberGames=0
+
+
 
 function playRound(playerSelection,computerSelection) {
     if (playerSelection === computerSelection) {
         result.textContent='It\'s a tie' ;
     } else if (playerSelection === 'Paper') {
-        if (computerSelection === 'Rock') {
 
+        if (computerSelection === 'Rock') {
             result.textContent ='You won this round! :)';
+            userScore +=1;
+            scoreuser.textContent = 'User : '+userScore;
+
         } else if (computerSelection === 'Scissors') {
             result.textContent ='You lost this round... :(';
+            computerScore +=1;
+            scorecomputer.textContent = 'Computer : ' +computerScore;
         }
     } else if (playerSelection === 'Rock') {
         if (computerSelection === 'Scissors') {
             result.textContent = 'You won this round! :)';
+            userScore +=1;
+            scoreuser.textContent = 'User : '+userScore;
+
         } else if (computerSelection === 'Paper') {
             result.textContent = 'You lost this round... :(';
+            computerScore +=1;
+            scorecomputer.textContent = 'Computer : ' +computerScore;
         }
     } else if (playerSelection === 'Scissors') {
         if (computerSelection === 'Paper') {
             result.textContent= 'You won this round! :)';
-        } else if (computerSelection === 'Rock') {
+            userScore +=1;
+            scoreuser.textContent = 'User : '+userScore;
+
+         } else if (computerSelection === 'Rock') {
             result.textContent= 'You lost this round... :(';
+            computerScore +=1;
+            scorecomputer.textContent = 'Computer : ' +computerScore;
         }
     }
 }
+
 
 function game() {
     let userWins = 0;
@@ -108,11 +133,57 @@ buttons.forEach(button => {
     button.addEventListener('mouseup', function (e) {
         e.target.classList.remove('click');
     });
+    button.addEventListener('mouseup',endgame);
 });
 
 const rock = document.querySelector('.rock');
-rock.addEventListener('click', function(){(playround_one_arg('Rock'))})
+rock.addEventListener('click', function(){(playround_one_arg('Rock'))});
 const paper = document.querySelector('.paper');
-paper.addEventListener('click', function(){(playround_one_arg('Paper'))})
+paper.addEventListener('click', function(){(playround_one_arg('Paper'))});
 const scissors = document.querySelector('.scissors');
-scissors.addEventListener('click', function(){(playround_one_arg('Scissors'))})
+scissors.addEventListener('click', function(){(playround_one_arg('Scissors'))});
+
+let nbgames = document.querySelector('.games');
+
+const winner = document.querySelector('.final')
+
+function endgame() {
+    if (numberGames<5) {
+        numberGames+=1;
+        nbgames.textContent = numberGames;
+    }
+    if (numberGames==5) {
+        if (userScore> computerScore) {
+            winner.classList.add('winner');
+            winner.textContent='You won! :)';
+        } else {
+            winner.classList.add('winner');
+            winner.textContent='You lost! :(';
+        }        
+        userScore=0;
+        computerScore=0;
+        numberGames=0;
+        nbgames.textContent = numberGames;
+        scorecomputer.textContent = userScore;
+        scoreuser.textContent = computerScore;
+    } 
+}
+
+function removeResult() {
+    winner.textContent=''
+}
+
+winner.addEventListener('mouseenter', function (e) {
+    e.target.classList.add('hover');
+});
+
+winner.addEventListener('mouseout', function (e) {
+    e.target.classList.remove('hover');
+});
+
+winner.addEventListener('click', function(e) {
+    e.target.classList.remove('winner')
+});
+
+winner.addEventListener('click',removeResult);
+
